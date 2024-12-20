b676b69408ac6ce8fdaf9e4026c43ba05793cdde
角砂糖 <90336521+lings03@users.noreply.github.com>
2024-12-21 04:36:59 +0800

    Update dependency

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,15 +7,5 @@ pluginManagement {
     }
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-        maven { url = uri("https://jitpack.io") }
-    }
-}
-
 rootProject.name = "Mod Manager"
 include(":app")
